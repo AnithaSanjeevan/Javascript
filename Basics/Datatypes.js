4db23b890522b202b5a8d49be16b0454c61cc11e
AnithaSanjeevan <52465941+AnithaSanjeevan@users.noreply.github.com>
2024-11-17 12:36:34 +0000

    Datatypes

---
+++ Basics/Datatypes.js
@@ -0,0 +1,35 @@
+//alert("hello")
+//number
+//bigint
+//string
+//boolean
+//null
+//undefined
+// let a = 1
+// let b = 'S'
+// let c = true
+// let d = null
+// let e
+// console.log(typeof(a));
+
+// console.log(typeof(b));console.log(typeof(c));console.log(typeof(d));console.log(typeof(e));
+
+// let score =null
+
+// let numberscore = Number(score)
+// console.log(numberscore);
+// console.log(typeof(numberscore));
+
+// let score =undefined
+
+// let stringscore = String(score)
+// console.log(stringscore);
+// console.log(typeof(stringscore));
+
+let score =0
+
+let booleanscore = Boolean(score)
+console.log(booleanscore);
+console.log(typeof(booleanscore));
+
+
---
+++ Basics/Variables.js
@@ -0,0 +1,7 @@
+//property = 'anitha'
+const variable2 = "Jeevitha"
+var variable1 = "Pagal"
+let variable3 = "Tuhin"
+//console.log(property)
+//let property
+console.table([variable1,variable2,variable3])
