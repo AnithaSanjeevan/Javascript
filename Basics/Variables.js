d61bdad52c3b6405d65cefe8a8f790304995861d
AnithaSanjeevan <52465941+AnithaSanjeevan@users.noreply.github.com>
2024-11-18 15:55:32 +0000

    todays commit

--- Basics/Variables.js
+++ Basics/Variables.js
@@ -1,7 +1,11 @@
-//property = 'anitha'
-const variable2 = "Jeevitha"
-var variable1 = "Pagal"
-let variable3 = "Tuhin"
-//console.log(property)
-//let property
-console.table([variable1,variable2,variable3])
+// //property = 'anitha'
+// const variable2 = "Jeevitha"
+// var variable1 = "Pagal"
+// let variable3 = "Tuhin"
+// //console.log(property)
+// //let property
+// console.table([variable1,variable2,variable3])
+
+console.log("1"+"5"+2);//152
+console.log(1+2+"2");//32
+
