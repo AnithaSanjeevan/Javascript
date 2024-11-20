060735fb27fe6adb80c537da4cce0edd1541e641
AnithaSanjeevan <52465941+AnithaSanjeevan@users.noreply.github.com>
2024-11-20 15:06:08 +0000

    today

--- Basics/Variables.js
+++ Basics/Variables.js
@@ -6,6 +6,21 @@
 // //let property
 // console.table([variable1,variable2,variable3])
 
-console.log("1"+"5"+2);//152
-console.log(1+2+"2");//32
+// console.log("1"+"5"+2);//152
+// console.log(1+2+"2");//32
+// let i;
+// for(i=0 ; i<10 ;i++)
+// {
+//     console.log(i);
+//   setTimeout(
+//     function()
+//     {
+//         console.log(i);
+//     },1000)
+// }
+
+const i = "a"
+const j = "a"
+
+console.log(i==j);
 
