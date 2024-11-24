c69d55af85dd47f979967a85ffb7207cbd9968ae
AnithaSanjeevan <52465941+AnithaSanjeevan@users.noreply.github.com>
2024-11-24 06:58:55 +0000

    test

---
+++ Basics/String1.js
@@ -0,0 +1,26 @@
+// const str = "Anitha"
+// let str1 = str
+// //str1 = "Jeevitha"
+// console.log(str);
+// console.log([]);
+
+let str = new String("Anitha")
+console.log(str[1]);
+console.log(str);
+console.log(typeof(str));
+
+const strnew = "Anitha"
+//console.log(strnew.);
+console.log(strnew.substring(1,3));
+console.log(strnew.slice(-4,4));
+
+
+
+
+
+
+
+
+
+
+
